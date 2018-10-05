340ea306763dd2a8a8d72399a1f01d72e8d35313
PKraakman <pkraakman@shindigz.com>
2018-10-05 17:24:29 -0400

    startingon excercises

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,16 +13,24 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie()
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
             {
-                Name = "Shrek!"
+                new Customer {Name = "Customer 11"},
+                new Customer {Name = "Customer 22"}
             };
 
-            //return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
 
             //return Content("hoi everyone");
             //return HttpNotFound();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = 2 });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = 2 });
         }
 
         public ActionResult Edit(int id)
@@ -29,9 +38,24 @@ namespace Vidly.Controllers
             return Content("MovieId:" + id.ToString());
         }
 
-        private ActionResult Content(string v, int id)
+        public ActionResult Index(int? pageIndex, string sortBy)
         {
-            throw new NotImplementedException();
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("Showing page {0}, sorted by {1}", pageIndex, sortBy));
         }
+
+
+        [Route("movies/released/{year:regex(2017|2018)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("Movies Released Since {0} / {1}", month, year));
+        }
+
+
     }
 }
